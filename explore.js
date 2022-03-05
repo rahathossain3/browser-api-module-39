17047ddd42b9310d0344e0caa248e8dc517975b9
rahathossain3 <rahathossain210@gmail.com>
2022-03-06 03:53:07 +0600

    browser API

---
+++ explore.js
@@ -0,0 +1,8 @@
+const name = 15;
+const student = {
+    name: 'jolil', friend: 'borsna', job: 'Business', makeMovie: function (name) {
+        console.log(name);
+    }
+};
+student.makeMovie('Din raat');
+document.getElementById('name');
